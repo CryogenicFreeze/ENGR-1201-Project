b9b34f6bad2cab78d0eeb8a24790e9a820890443
CryogenicFreeze <20272772+CryogenicFreeze@users.noreply.github.com>
2024-03-08 23:26:47 -0600

    Added ease and stop functions

--- main/main.ino
+++ main/main.ino
@@ -1,52 +1,78 @@
-
 #include <Adafruit_MotorShield.h>
 Adafruit_MotorShield myMotorShield = Adafruit_MotorShield();
 
-Adafruit_DCMotor *Left_F = myMotorShield.getMotor(1); //left front
-Adafruit_DCMotor *Right_B = myMotorShield.getMotor(2); //right back
-Adafruit_DCMotor *Right_F = myMotorShield.getMotor(3); //right front
-Adafruit_DCMotor *Left_B = myMotorShield.getMotor(4); //left back
+Adafruit_DCMotor *Front_Left = myMotorShield.getMotor(1); //M1 port
+Adafruit_DCMotor *Back_Right = myMotorShield.getMotor(2); //M2 port
+Adafruit_DCMotor *Front_Right = myMotorShield.getMotor(3); //M3 port
+Adafruit_DCMotor *Back_Left = myMotorShield.getMotor(4); //M4 port
 
-void setup() {
+void setup()
+{
   myMotorShield.begin();
 }
 
-void loop() {
-  direction(0);
-  set_speed(125);
+void loop()
+{
+  forward_backward(0);
+  set_speed(accel_ease(0.1, 100, 250));
 }
 
-void set_speed(int speed){
-  Left_F->setSpeed(speed);
-  Left_B->setSpeed(speed);
-  Right_F->setSpeed(speed);
-  Right_B->setSpeed(speed);
- }
-
-void direction(int val){ // forward val = 0 backward val = 1
-  if (val = 0){
-      Left_F->run(FORWARD);
-      Right_B->run(FORWARD);
-      Right_F->run(FORWARD);
-      Left_B->run(FORWARD);
-    } else if (val = 1){
-      Left_F->run(BACKWARD);
-      Right_B->run(BACKWARD);
-      Right_F->run(BACKWARD);
-      Left_B->run(BACKWARD);
-      }
- }
-
-void turn(int l_r){ //turn right l_r=0 turn left l_r=1
-  if (l_r = 0){
-      Left_F->run(FORWARD);
-      Right_B->run(BACKWARD);
-      Right_F->run(BACKWARD);
-      Left_B->run(FORWARD);
-    } else if (l_r = 1) {
-      Left_F->run(BACKWARD);
-      Right_B->run(FORWARD);
-      Right_F->run(FORWARD);
-      Left_B->run(BACKWARD);
-      }
+int accel_ease(float dx, int wait_time, int max_speed) //declare dx step value and time between loops in ns, and max motor speed(less than 255)
+{
+  float y;
+  for (float x = 0; x <= 1.5; x += dx) 
+  {
+    y = max_speed*(pow(x, 2) - (pow(x, 4)/3) + ((2*pow(x, 6))/45) - (pow(x, 8)/315)); //easing function
+    return y;
+    delay(wait_time);
   }
+}
+
+void stop()
+{
+  Front_Left->run(RELEASE);
+  Back_Left->run(RELEASE);
+  Front_Right->run(RELEASE);
+  Back_Right->run(RELEASE);
+}
+
+void set_speed(int speed) // max 255
+{
+  Front_Left->setSpeed(speed);
+  Back_Left->setSpeed(speed);
+  Front_Right->setSpeed(speed);
+  Back_Right->setSpeed(speed);
+ }
+
+void forward_backward(int val){ // 0 for forward, 1 for backward
+  if (val = 0)
+    {
+      Front_Left->run(FORWARD);
+      Back_Right->run(FORWARD);
+      Front_Right->run(FORWARD);
+      Back_Left->run(FORWARD);
+    } else if (val = 1)
+    {
+      Front_Left->run(BACKWARD);
+      Back_Right->run(BACKWARD);
+      Front_Right->run(BACKWARD);
+      Back_Left->run(BACKWARD);
+    }
+ }
+
+void left_right(int val) // 0 for right turn, 1 for left turn
+{ 
+  if (val = 0)
+  {
+      Front_Left->run(FORWARD);
+      Back_Left->run(FORWARD);
+      Front_Right->run(BACKWARD);
+      Back_Right->run(BACKWARD);
+  } else if (val = 1)
+  {
+      Front_Left->run(BACKWARD);
+      Back_Right->run(FORWARD);
+      Front_Right->run(FORWARD);
+      Back_Left->run(BACKWARD);
+  }
+}
