bc9e45848a35507ffa83fe1f636e308123769d22
CryogenicFreeze <20272772+CryogenicFreeze@users.noreply.github.com>
2024-05-02 22:49:35 -0500

    added checking sides while running logic

--- main/main.ino
+++ main/main.ino
@@ -3,7 +3,7 @@
 #include <Servo.h>
 #include <IRremote.h>
 
-// Photocell ======================================================
+// leds ======================================================
 const int photocellPin = 0;
 const int led_pin_R = 6;
 const int led_pin_G = 5;
@@ -25,6 +25,7 @@ float left_dist;
 float right_dist;
 float l_wall_dist;
 float r_wall_dist;
+float f_dist;
 NewPing sonar(trigPin, echoPin, 200); // initialize the sonar sensor and set max distance to 200cm
 
 //setup servo =====================================================
@@ -38,7 +39,7 @@ Adafruit_DCMotor *Back_Right = myMotorShield.getMotor(2); //M2 port
 Adafruit_DCMotor *Front_Right = myMotorShield.getMotor(3); //M3 port
 Adafruit_DCMotor *Back_Left = myMotorShield.getMotor(4); //M4 port
 
-// init setup =====================================================
+// init ===========================================================
 void setup() {
   Serial.begin(9600); //set serial output to 9600 baud
   myMotorShield.begin(); //start the dc motor shield library
@@ -54,9 +55,8 @@ void setup() {
 // main loop ======================================================
 void loop() {
   if (is_running == 1){
-    //check_infront();
-    check_sides();
-    led_color(0,255,0);
+    check_for_obstacles();
+    //led_color(0,255,0);
   } else {
     halt();
   }
@@ -70,7 +70,7 @@ void turn(int v, float time) {
   delay(time);
 }
 
-// photocell function =============================================
+// led function =============================================
 void led_color(int red, int green, int blue) {
   int photocellReading = 1023 - analogRead(photocellPin);
   led_brightness = map(photocellReading, 0, 1023, 0, 255);
@@ -84,75 +84,70 @@ void led_color(int red, int green, int blue) {
   }
 }
 
-// turn servo left or right =======================================
-void turn_sensor(int val) {
-  if (val == 0) // 0 for 180 degrees 
-  {
-    myservo.write(180);
-  } else if (val == 1) // 1 for 0 degrees
-  {
-    myservo.write(0);
-  }
-}
-
 // check for ir remote signal =====================================
 void signal_check() {
   if (irrecv.decode(&results)) {
-    Serial.println(results.value, HEX);
     if (results.value == 0XFFFFFFFF) { //block external values
       is_running = !is_running; // flip is_running boolean
-      Serial.println(is_running);
       delay(100);
     }
     irrecv.resume();
   }
 }
 
-// check on either side ===========================================
-void check_sides() {
-  myservo.write(60);
-  delay(250);
-  l_wall_dist = get_distance() * (sqrt(3)/2);
-  myservo.write(120);
-  delay(250);
-  r_wall_dist = get_distance() * (sqrt(3)/2);
-  if (l_wall_dist < 30 and r_wall_dist == 0){
-    turn(0, 225);
-  } else if (r_wall_dist < 30 and l_wall_dist == 0){
-    turn(1, 255);
+void check_if_close(float dist) {
+  if (dist < 40 and dist != 0) {
+    stop(100);
+    reroute();
+  } else {
+    forward_backward(0);
+    set_speed(100);
   }
 }
 
-// check for obstacles ============================================
-void check_infront() {
-  get_distance();
-  if ((distance < 30) and (distance != 0))
-  {
-    stop(100);
-    led_color(255,0,0);
-    reroute();
-  } else
-  {
-    forward_backward(0);
-    set_speed(100);
-    return;
-  }
+void check_for_obstacles() {
+
+  myservo.write(150);
+  delay(100);
+  l_wall_dist = get_distance();
+  check_if_close(l_wall_dist);
+  delay(100);
+
+  myservo.write(90);
+  delay(100);
+  f_dist = get_distance();
+  check_if_close(f_dist);
+  delay(100);
+
+  myservo.write(30);
+  delay(100);
+  r_wall_dist = get_distance();
+  check_if_close(r_wall_dist);
+  delay(100);
+
+  myservo.write(90);
+  delay(100);
+  f_dist = get_distance();
+  check_if_close(f_dist);
+  delay(100);
+
 }
 
 // get distance from sensor ========================================
-int get_distance() {
+float get_distance() {
   duration = sonar.ping_median(5);
   distance = duration * 0.034/2;
+  Serial.print(distance);
   return distance;
-}
+  }
 
 // rerouting logic =================================================
 void reroute() {
-  turn_sensor(0);
+  myservo.write(180);
   delay(250);
   left_dist = get_distance(); // turn sensor to the left and check the distance 
   delay(250);
-  turn_sensor(1);
+  myservo.write(0);
   delay(250);
   right_dist = get_distance(); // turn sensor to the right and check distance
   delay(250);
@@ -161,9 +156,9 @@ void reroute() {
   if (abs(left_dist - right_dist) < 5 and (left_dist != 0) and (right_dist != 0)) { // if the distances are close and not = 0 turn 180
     turn(0, 1070); // turn 180
   } else if (left_dist < right_dist) { // if the distances are non similar and the left is closer turn right
-    turn(0, 535); // turn right
+    turn(0, 435); // turn right
   } else {
-    turn(1, 535); //turn left
+    turn(1, 435); //turn left
   }
 }
 
@@ -174,7 +169,7 @@ void stop(float time) {
   Front_Right->run(RELEASE);
   Back_Right->run(RELEASE);
   delay(time);
-}
+  }
 
 // completely stop car ==========================================
 void halt() {
@@ -182,7 +177,7 @@ void halt() {
   Back_Left->run(RELEASE);
   Front_Right->run(RELEASE);
   Back_Right->run(RELEASE);
-}
+  }
 
 // function to set car speed ====================================
 void set_speed(int speed) { // max 255
@@ -190,16 +185,16 @@ void set_speed(int speed) { // max 255
   Back_Left->setSpeed(speed+8);
   Front_Right->setSpeed(speed);
   Back_Right->setSpeed(speed);
- }
+  }
 
 // function to set drive direction to forward or backward =======
-void forward_backward(int val){ // 0 for forward, 1 for backward
+void forward_backward(int val) { // 0 for forward, 1 for backward
   if (val == 0){
       Front_Left->run(FORWARD);
       Back_Right->run(FORWARD);
       Front_Right->run(FORWARD);
       Back_Left->run(FORWARD);
-    } else if (val == 1){
+    } else if (val == 1) {
       Front_Left->run(BACKWARD);
       Back_Right->run(BACKWARD);
       Front_Right->run(BACKWARD);
@@ -214,10 +209,10 @@ void left_right(int val) { // 0 for right turn, 1 for left turn
       Back_Left->run(FORWARD);
       Front_Right->run(BACKWARD);
       Back_Right->run(BACKWARD);
-  } else if (val == 1) {
+    } else if (val == 1) {
       Front_Left->run(BACKWARD);
       Back_Right->run(FORWARD);
       Front_Right->run(FORWARD);
       Back_Left->run(BACKWARD);
+    }
   }
-}
